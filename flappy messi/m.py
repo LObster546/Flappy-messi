45575cf4e076b12323fd10a4bba0c9427581e3a2
Смирнов Георгий <166622737+LObster546@users.noreply.github.com>
2026-01-22 13:50:56 +0300

    Add files via upload

--- flappy messi/m.py
+++ flappy messi/m.py
@@ -28,10 +28,12 @@ class PLayer(GameSprite):
 
 class Prop(GameSprite):
     def update(self):
-        side = random.randint(1,2)
-        if side == 1:
-            pass
-        pass
+        # side = random.randint(1,2)
+        # if side == 1:
+        #     self.rect.y = randint(-10, 30)
+        # else:
+        #     self.rect.y = randint(900, 950)
+        self.rect.x -= self.speed
 
 
 
@@ -48,12 +50,29 @@ display.set_caption('Flappy Messi')
 
 bg = GameSprite('fon.png', 1200, 900, 0, 0, 0)
 
-pl = PLayer('p1.jpg', 100, 200, 70, 300, 3 )
+pl = PLayer('p1.jpg', 100, 200, 70, 300, 5)
+
+igrok = sprite.Group(pl)
+pr = sprite.Group()
+
+for i in range(10):
+    side = randint(1,2)
+    if side == 1:
+        vis = randint(-10, 5)
+    else:
+        vis = randint(880, 950)
+    lim = Prop('lim.jpg', 125, randint(100, 150), 1200, vis, 5 )
+    pr.add(lim)
+
+
+
 
 font.init()
 font = font.Font(None, 70)
 
 score_t = None 
+cd = 0
+
 
 s_txt = font.render(score_t, True, (255, 255 , 0))
 lose = font.render('Ты проиграл ', True, (255, 0, 0))
@@ -67,7 +86,9 @@ FPS = 60
 game = True
 
 while game:
+
     if not over:
+        cd -= 1
         t -= 1
         if t <= 0:
             score += 1
@@ -76,15 +97,32 @@ while game:
             s_txt = font.render(score_t, True, (255, 255 , 0))
 
 
+        
+
         bg.update()
         bg.reset()
 
+        igrok.update()
+        igrok.draw(window)
 
+        pr.update()
+        pr.draw(window)
+        if cd <= 0:
+            side = randint(1,2)
+            if side == 1:
+                vis = randint(-10, 5)
+            else:
+                vis = randint(700, 800)
+            lim = Prop('lim.jpg', 125, randint(200, 600), 1200, vis, 5 )
+            pr.add(lim)
+            cd = randint(65, 71)
+        
 
-        pl.update()
-        pl.reset()
-
-        if pl.rect.y >= 700:
+        if sprite.groupcollide(igrok, pr, False, False):
+            over = True
+            window.blit(lose, (450, 350))
+            window.blit(s_txt,(450, 450))
+        if (pl.rect.y >= 700) or (pl.rect.y <= -50):
             over = True
             window.blit(lose, (450, 350))
             window.blit(s_txt,(450, 450))
